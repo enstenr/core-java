ae44ee0509386de1e103eab38d305b73cce0a006
bigthinker <rajesh@giboo.com>
2023-07-14 18:14:31 +0530

    updated code

--- core-java/src/day1/MainProgram.java
+++ core-java/src/day1/MainProgram.java
@@ -3,7 +3,11 @@ package day1;
 public class MainProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num=1;
+		boolean val=true;
+		char c='c';
+		float f=1.2f;
+		double d=1.2;
 		System.out.println(" Welcome Rajesh");
 	}
 
