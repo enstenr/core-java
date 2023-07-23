9ea3ecbc213034a39f5180c4037789b3c0b8d7dc
bigthinker <rajesh@giboo.com>
2023-07-23 15:52:42 +0530

    primitive datatypes

--- core-java/src/day1/MainProgram.java
+++ core-java/src/day1/MainProgram.java
@@ -1,14 +1,23 @@
 package day1;
 
 public class MainProgram {
-
+	//bbc dfils
+// byte (8 bit) ,boolean and char, double, float, int , long , short
+//data types - > 
+	// 10, 19th cross, 60000
+	// 1+2=3
+	//1.5+3.1=4.6 
+	
+	
+	//data types
 	public static void main(String[] args) {
-		int num=1;
+		byte b=127;// 8 bit -127 -to +127 
+		int num=999999999;
 		boolean val=true;
-		char c='c';
-		float f=1.2f;
-		double d=1.2;
-		System.out.println(" Welcome Rajesh");
+		char c='+';
+		float f=15.2111f;
+		double d=1.2456;
+		System.out.println(" Welcome to Java"+d);
 	}
 
 }
